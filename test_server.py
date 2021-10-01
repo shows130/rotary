7abca10a8426605108a2d6dbbfdee6e9958a9b9b
esh1122 <84693788+esh1122@users.noreply.github.com>
2021-10-01 16:56:29 +0900

    주석처리

--- test_server.py
+++ test_server.py
@@ -1,35 +1,39 @@
 # TCP server example
-import sys
+import sys          
 import socket
 import logging
 from utils import calc_checksum
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-logger = logging.getLogger('server')
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)         #로그 기본 설정(초기시작문자, 단계)
+logger = logging.getLogger('server')                                #'server'의 로그를 받아온다
 
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #소켓 생성(주소 패밀리, 소켓 유형) 
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #소켓기본 설정
 
-server_socket.bind(("", 5090))
-server_socket.listen(5)
+server_socket.bind(("", 5090))  #소켓 주소 정보 할당(ip,port)
+server_socket.listen(5)         #연결 수신 대기 : 최대 5개까지 
 
 print("TCPServer Waiting for client on port 5090")
 
 while True:
-    client_socket, address = server_socket.accept()
-    logger.info(f"I got a connection from {address}")
+    client_socket, address = server_socket.accept()         #client소켓과 주소를 튜플로 받아온다
+    logger.info(f"I got a connection from {address}")       #info level의 log출력
     while True:
         try:
-            data = client_socket.recv(512)
-            logger.info(f'received: {data}')
+            data = client_socket.recv(512)                  #0.5kb까지 소켓통신을 통해 받아온다
+            logger.info(f'received: {data}')                #log를 통해 받은 data를 출력
+            #data의 길이가 22가 아닐경우 오류 알림
             if 22 != len(data):
                 raise ValueError(f'data is not expected size (22), but {len(data)}')
+            #변조 확인 : data를 계산
             checksum = calc_checksum(data[1:20])
+            #수신된 data내부의 변조확인항과 계산한 변조확인항을 비교
+            #변조확인 결과가 같지 않을 경우 예외 발생
             if data[20].to_bytes(1, 'big') != checksum:
-                raise ValueError('checksum error.')
+                raise ValueError('checksum error.')         #값의 형태는 에러가 아니나 원하는 값이 아닐 경우 사용하는 에러 발생 코드 
         except Exception as e:
-            logger.error(str(e))
+            logger.error(str(e))            #error level의 log출력
         finally:
-            client_socket.close()
+            client_socket.close()           #client socket을 닫는다
             break
--- utils.py
+++ utils.py
@@ -1,18 +1,22 @@
+# 변조확인항 계산
 def calc_checksum(data):
-    checksum = sum(data) % 256
-    checksum = checksum.to_bytes(1, byteorder='big')
+    checksum = sum(data) % 256 #data값을 전부 더하고 256으로 나눈 나머지를 반환
+    checksum = checksum.to_bytes(1, byteorder='big') #반환된 숫자를 1byte로 표현(읽는 방식 = 왼쪽부터)
     return checksum
-
 
 def get_protocol_data(dt, data):
     # data = b'2103011012349999999'
-    header = b'\x02'
-    tail = b'\x03'
-    data = bytes(f'{dt}{data}', 'utf-8')
+    header = b'\x02' #프로토콜 시작을 알리는 항
+    tail = b'\x03' #프로토콜 끝을 알리는 항
+    data = bytes(f'{dt}{data}', 'utf-8') #시간과 각도값을 utf-8형식으로 바이트 형태로 인코딩
+    #data의 길이가 19가 아닐경우 에러 문구 생성
     if len(data) != 19:
         raise ValueError(f'expected length is 19 but {len(data)}')
+    #변조확인항 생성
     checksum = sum(data) % 256
     checksum = checksum.to_bytes(1, byteorder='big')
+
+    #최종적으로 발신할 data 생성
     data = header + data + checksum + tail
     return data
 
